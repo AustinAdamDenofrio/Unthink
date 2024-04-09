9587b7f4029003d4ce9ce4770fe50ea556d5b3df
Austin Denofrio DT <denofrioaustin@gmail.com>
2024-04-09 18:15:09 -0400

    Created the framework for the unthink app and made a debugger for it

--- js/site.js
+++ js/site.js
@@ -1,16 +1,60 @@
-//Display the value property of input id to alert
+// Collect user inputs
+// AKA the Entry Point
+function getValues() {
+  // get an input element for startNumber and grab its value.
+  let startNumber = document.getElementById('startNumber').value;
 
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    let message = inputBox.value;
+  // Do the same for endNumber
+  let endNumber = document.getElementById('endNumber').value;
 
-    Swal.fire({
-        title: "Unthink",
-        text: message,
-        icon: "success",
-        backdrop: false
-    
-      });
-      
+  // HTML strings need the data type converted to integers. 
+  // This is the constructor
+  startNumber = Number(startNumber)
+  endNumber = Number(endNumber)
+
+  // run the function, passing the numbers, itll make an array and itll pass back an array.
+  let generatedNumbers = generateValues(startNumber, endNumber);
+
+  displayValues(generatedNumbers);
+
 }
 
+
+
+
+
+// generate a list of numbers between those 2 values
+function generateValues(start, end) {
+  let numbers = [];
+
+  for (let i = start; i <= end; i = i + 1) {
+      numbers.push(i);
+  }
+
+  return numbers;
+}
+
+
+
+
+
+
+
+// display in results table
+function displayValues(numberArray) {
+
+  // numberArray = [0,1,2,3,....]
+  let tableHtml = '';
+
+  for (let index = 0; index < numberArray.length; index = index + 1) {
+
+    let number = numberArray[index];
+
+    tableHtml = tableHtml + '<tr><td>' + number + '</td></tr>';
+
+  }
+
+  let tbody = document.getElementById('results');
+
+  tbody.innerHTML = tableHtml;
+}
